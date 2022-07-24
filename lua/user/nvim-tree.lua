084c26c0e43d6ef76a9b2b218a1ba344fe45b7d7
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-07-25 00:20:45 +0300

    update nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -15,38 +15,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 
 
 nvim_tree.setup {
-	auto_reload_on_write = true,
-	create_in_closed_folder = false,
-	disable_netrw = false,
-	hijack_cursor = false,
-	hijack_netrw = true,
-	hijack_unnamed_buffer_when_opening = false,
-	ignore_buffer_on_setup = false,
-	open_on_setup = false,
-	open_on_setup_file = false,
-	open_on_tab = false,
-	sort_by = "name",
-	update_cwd = false,
-	reload_on_bufenter = false,
-	respect_buf_cwd = false,
-	view = {
-		adaptive_size = false,
-		centralize_selection = false,
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		preserve_window_proportions = false,
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
-		mappings = {
-			custom_only = false,
-			list = {
-				-- user mappings go here
-			},
-		},
-	},
 	renderer = {
 		add_trailing = false,
 		group_empty = false,
@@ -57,10 +25,10 @@ nvim_tree.setup {
 		indent_markers = {
 			enable = false,
 			icons = {
-				corner = "└ ",
-				edge = "│ ",
-				item = "│ ",
-				none = "  ",
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
 			},
 		},
 		icons = {
@@ -77,6 +45,7 @@ nvim_tree.setup {
 			glyphs = {
 				default = "",
 				symlink = "",
+				bookmark = "",
 				folder = {
 					arrow_closed = "",
 					arrow_open = "",
@@ -88,7 +57,8 @@ nvim_tree.setup {
 					symlink_open = "",
 				},
 				git = {
-					unstaged = "",
+ 					unstaged = "",
+					-- unstaged = "✗",
 					staged = "✓",
 					unmerged = "",
 					renamed = "➜",
@@ -99,90 +69,6 @@ nvim_tree.setup {
 			},
 		},
 		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-	},
-	hijack_directories = {
-		enable = true,
-		auto_open = true,
-	},
-	update_focused_file = {
-		enable = false,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	ignore_ft_on_setup = {},
-	system_open = {
-		cmd = "",
-		args = {},
-	},
-	diagnostics = {
-		enable = false,
-		show_on_dirs = false,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	filters = {
-		dotfiles = false,
-		custom = {},
-		exclude = {},
-	},
-	filesystem_watchers = {
-		enable = false,
-		interval = 100,
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 400,
-	},
-	actions = {
-		use_system_clipboard = true,
-		change_dir = {
-			enable = true,
-			global = false,
-			restrict_above_cwd = false,
-		},
-		expand_all = {
-			max_folder_discovery = 300,
-		},
-		open_file = {
-			quit_on_open = false,
-			resize_window = true,
-			window_picker = {
-				enable = true,
-				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-				exclude = {
-					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-					buftype = { "nofile", "terminal", "help" },
-				},
-			},
-		},
-		remove_file = {
-			close_window = true,
-		},
-	},
-	trash = {
-		cmd = "gio trash",
-		require_confirm = true,
-	},
-	live_filter = {
-		prefix = "[FILTER]: ",
-		always_show_folders = true,
-	},
-	log = {
-		enable = false,
-		truncate = false,
-		types = {
-			all = false,
-			config = false,
-			copy_paste = false,
-			diagnostics = false,
-			git = false,
-			profile = false,
-			watcher = false,
-		},
+		symlink_destination = true,
 	},
 }
