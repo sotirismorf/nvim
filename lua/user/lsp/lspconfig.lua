6b3370e375ad09e9e9ef5b408647607463cbec1d
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-10-14 16:38:40 +0300

    separate lspconfig file

---
+++ lua/user/lsp/lspconfig.lua
@@ -0,0 +1,25 @@
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
+    -- on_attach = require("user.lsp.handlers").on_attach,
+    -- capabilities = require("user.lsp.handlers").capabilities,
+  }
+
+  if server == "jsonls" then
+    local jsonls_opts = require "user.lsp.settings.jsonls"
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
+  if server == "sumneko_lua" then
+    local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  lspconfig[server].setup(opts)
+end
