66432fa91d6043e1b5276b77181fcaca97c1e62e
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-12-22 16:21:35 +0200

    update nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,4 +1,3 @@
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
 	return
@@ -6,56 +5,14 @@ end
 
 nvim_tree.setup {
 	renderer = {
-		add_trailing = false,
-		group_empty = false,
-		highlight_git = false,
-		full_name = false,
-		highlight_opened_files = "none",
-		root_folder_modifier = ":~",
+		highlight_opened_files = "name",
+		root_folder_label = ":~",
 		indent_markers = {
 			enable = true,
-			icons = {
-				corner = "└",
-				edge = "│",
-				item = "│",
-				none = " ",
-			},
 		},
 		icons = {
-			webdev_colors = true,
-			git_placement = "before",
-			padding = " ",
-			symlink_arrow = " ➛ ",
 			show = {
-				file = true,
-				folder = true,
-				folder_arrow = true,
-				git = true,
-			},
-			glyphs = {
-				default = "",
-				symlink = "",
-				bookmark = "",
-				folder = {
-					arrow_closed = "",
-					arrow_open = "",
-					default = "",
-					open = "",
-					empty = "",
-					empty_open = "",
-					symlink = "",
-					symlink_open = "",
-				},
-				git = {
- 					unstaged = "",
-					-- unstaged = "✗",
-					staged = "✓",
-					unmerged = "",
-					renamed = "➜",
-					untracked = "★",
-					deleted = "",
-					ignored = "◌",
-				},
+				folder_arrow = false,
 			},
 		},
 		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
