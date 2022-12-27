7712c1a2b979f96b50648334af67ecbd84d15049
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-12-27 22:57:30 +0200

    colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,26 +1,2 @@
-vim.cmd [[
-try
-	colorscheme gruvbox
-	set background=dark
-	" hi normal       ctermbg=NONE guibg=NONE
-	" hi vertsplit    ctermbg=NONE guibg=NONE
-	" hi cursorline   ctermbg=NONE guibg=NONE
-	" hi cursorlinenr ctermbg=NONE guibg=NONE
-	" hi signcolumn   ctermbg=NONE guibg=NONE
-	" hi link gitgutteradd gruvboxgreen 
-	" hi link gitgutterchange gruvboxblue 
-	" hi link gitgutterdelete gruvboxred 
-catch /^Vim\%((\a\+)\)\=:E185/
-	colorscheme default
-  	set background=dark
-endtry
-]]
-
--- Background
---   hi normal ctermbg=NONE
--- Line between buffers
---   hi vertsplit ctermfg=fg ctermbg=NONE cterm=NONE
--- Unfocused buffer's statusline
--- hi statuslinenc cterm=NONE ctermbg=NONE ctermfg=fg 
--- Focused buffer's statusline
--- hi statusline cterm=NONE ctermbg=NONE ctermfg=fg
+require("gruvbox").setup()
+vim.cmd("colorscheme gruvbox")
