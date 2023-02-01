cf8891602c5521a820ee93c0d52941b24583b6fc
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-02-01 18:56:07 +0200

    update lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -23,6 +23,8 @@ mason_lspconfig.setup {
 	ensure_installed = servers
 }
 
+require("neodev").setup()
+
 local lspconfig = require("lspconfig")
 
 for _, server in pairs(servers) do
@@ -31,12 +33,26 @@ end
 
 require("lspconfig.ansible")
 
+require('lspconfig').yamlls.setup {
+	settings = {
+		yaml = {
+			schemas = {
+				["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
+				-- ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.yml",
+				-- ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "*.yml",
+			},
+		},
+	}
+}
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 local null_ls = require("null-ls")
 null_ls.setup {
 	debug = false,
 	sources = {
-		null_ls.builtins.diagnostics.yamllint,
+		null_ls.builtins.diagnostics.yamllint.with {
+			extra_args = { "-c", vim.env.HOME .. '/.config/nvim/lua/lspconfig/yamllint.yml' }
+		},
 		-- null_ls.builtins.formatting.prettierd.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 	},
 }
