d1d8c70f486e283eba082ab45123bb9efb46889c
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-04-28 00:36:22 +0300

    enable colorizer

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -6,16 +6,9 @@ end
 local options = {
 	filetypes = { "*" },
 	user_default_options = {
-		RGB = true, -- #RGB hex codes
-		RRGGBB = true, -- #RRGGBB hex codes
-		names = false, -- "Name" codes like Blue oe blue
 		RRGGBBAA = true, -- #RRGGBBAA hex codes
-		rgb_fn = true, -- CSS rgb() and rgba() functions
-		hsl_fn = true, -- CSS hsl() and hsla() functions
-		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-		css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-		-- Available modes: foreground, background, virtualtext
-		mode = "background", -- Set the display mode.)
+		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 	}
 }
 
