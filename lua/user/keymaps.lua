930fe54fe1ec3b211319eb6710c6c9112010bc2a
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-09-11 02:56:53 +0300

    update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,33 +1,22 @@
 local opts = { noremap = true, silent = true }
 
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- Save buffer
-keymap("n", "<C-s>", ":w<CR>", opts)
+vim.keymap.set("n", "<C-s>", ":w<CR>", opts)
 
 -- Don't copy to clipboard when using "c"
-keymap("n", "c", '"_c', opts)
+vim.keymap.set("n", "c", '"_c', opts)
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
--- Toggle keymaps (i.e. greek/english)
-keymap("i", "<C-k>", "<C-6>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Telescope
 vim.keymap.set("n", "<Leader>s", require('telescope.builtin').find_files, { desc = "find files" })
@@ -52,33 +41,36 @@ vim.keymap.set("n", "<Leader>lI", "<cmd>Mason<cr>", { desc = "Mason Interface" }
 vim.keymap.set("n", "<Leader>li", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
 
 -- Buffer move left and right
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { noremap = true })
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { noremap = true })
+vim.keymap.set("n", "<C-q>", "<cmd>Bdelete<cr>", { noremap = true, desc = "delete buffer" })
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
+
 -- Press jk fast to enter
-keymap("i", "jj", "<ESC>", opts)
+vim.keymap.set("i", "jj", "<ESC>", opts)
+
+-- Toggle keymaps (i.e. greek/english)
+vim.keymap.set("i", "<C-k>", "<C-6>", opts)
 
 -- Visual --
+
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", opts)
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", opts)
+vim.keymap.set("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
-
--- Greek --
-keymap("i", "ξξ", "<ESC>", opts)
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
