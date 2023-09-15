59263f71a8d385abd54a71a9ebdc84ee9a1a4409
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-09-15 13:26:00 +0300

    autocommands update

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,8 +1,7 @@
 vim.cmd [[
 	augroup _general_settings
 		autocmd!
-		autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-		autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+		autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
 		autocmd BufWinEnter * :set formatoptions-=cro
 		autocmd FileType qf set nobuflisted
 		autocmd BufNewFile,BufRead */waybar/config setlocal filetype=jsonc
@@ -22,30 +21,76 @@ vim.cmd [[
 		autocmd FileType markdown setlocal spell spelllang=el,en_us
 	augroup end
 
-	augroup indent2spaces
+	" start in insert mode when opening a terminal
+	augroup custom_term_open
 		autocmd!
-		autocmd FileType json setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd FileType markdown setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd FileType svelte setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd FileType typescript setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd FileType yaml setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd filetype javascript setlocal shiftwidth=2 tabstop=2 expandtab
-		autocmd filetype toml setlocal shiftwidth=2 tabstop=2 expandtab
+		autocmd TermOpen * startinsert | set winfixheight
 	augroup end
 
+	" automatically resize split windows when terminal is resized
 	augroup _auto_resize
 		autocmd!
-		autocmd VimResized * tabdo wincmd = 
-	augroup end
-
-	augroup _alpha
-		autocmd!
-		autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+		autocmd VimResized * tabdo wincmd =
 	augroup end
 ]]
 
--- Autoformat
--- augroup _lsp
---	 autocmd!
---	 autocmd BufWritePre * lua vim.lsp.buf.formatting()
--- augroup end
+local function augroup(name)
+	return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- indent with 2 spaces
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("indent_with_2_spaces"),
+	pattern = {
+		"javascript",
+		"json",
+		"jsonc",
+		"markdown",
+		"svelte",
+		"toml",
+		"typescript",
+		"yaml",
+	},
+	callback = function()
+		vim.opt.expandtab = true
+		vim.opt.shiftwidth = 2
+		vim.opt.tabstop = 2
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function()
+		local exclude = { "gitcommit" }
+		local buf = vim.api.nvim_get_current_buf()
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
+			return
+		end
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
