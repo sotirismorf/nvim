4daecae3c3292a94a16ce2eb68d2d75b6792b8fc
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-11-25 20:57:56 +0200

    move neorg config to separate dir

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,25 +63,4 @@ return {
 			},
 		}
 	},
-	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("neorg").setup {
-				load = {
-					["core.defaults"] = {}, -- Loads default behaviour
-					["core.concealer"] = {}, -- Adds pretty icons to your documents
-					["core.summary"] = {},
-					["core.dirman"] = { -- Manages Neorg workspaces
-						config = {
-							workspaces = {
-								notes = "~/p/repos/git/notes",
-							},
-						},
-					},
-				},
-			}
-		end,
-	},
 }
