d4f001fc611e9ece4eb8ba4938815efa471219f9
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2023-11-25 21:40:59 +0200

    go to neorg index

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -9,6 +9,16 @@ return
 				["core.defaults"] = {}, -- Loads default behaviour
 				["core.concealer"] = {}, -- Adds pretty icons to your documents
 				["core.summary"] = {},
+				["core.promo"] = {},
+				-- ["core.keybinds"] = {
+				-- 	-- Defaults: https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+				-- 	-- How to setup user keybinds: https://github.com/nvim-neorg/neorg/wiki/User-Keybinds
+				-- 	config = {
+				-- 		hook = function(keybinds)
+				-- 			keybinds.map("norg", "n", "<leader>ni", "<cmd>Neorg index<CR>")
+				-- 		end,
+				-- 	}
+				-- },
 				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
